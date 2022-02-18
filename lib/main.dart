46d358fd68839ab3b1351eafb55d06f417a1adf3
joaoffnogueira <contact@joaonogueira.dev>
2022-02-18 14:34:12 -0300

    display transactions in cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:purple_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const PurpleExpenses());
@@ -14,15 +16,21 @@ class PurpleExpenses extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Purple Expenses'),
+      home: MyHomePage(title: 'Purple Expenses'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Supermercado', value: 30.65, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Academia', value: 69.99, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +49,46 @@ class MyHomePage extends StatelessWidget {
               child: Text('Gráfico'),
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
+          Column(
+            children: _transactions.map((transaction) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${transaction.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(transaction.date),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
