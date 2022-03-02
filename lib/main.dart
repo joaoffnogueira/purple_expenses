76ca7c7de99b38977f0f7a1b203df87dff3b97fd
joaoffnogueira <contact@joaonogueira.dev>
2022-03-02 15:53:32 -0300

    Prepare 1st beta version

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class PurpleExpenses extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Purple Expenses',
       theme: tema.copyWith(
         colorScheme: tema.colorScheme.copyWith(
